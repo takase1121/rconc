994a942d0c17ea4c587ef97b162621a49a85659c
Kian Kasad <kian@kasad.com>
2020-11-23 21:16:06 -0800

    add basic option parsing

--- rconc.c
+++ rconc.c
@@ -1,24 +1,66 @@
+#include <errno.h>
+#include <getopt.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define PROGRAM_NAME "rconc"
 #define VERSION      "0.0.1"
 
 enum packet_type {
-	PKT_RESPONSE      = 0,
+	PKT_RESPONSE = 0,
 	PKT_AUTH_RESPONSE = 2,
-	PKT_COMMAND       = 2,
-	PKT_AUTH          = 3,
+	PKT_COMMAND = 2,
+	PKT_AUTH = 3,
 };
 
 struct packet {
-	int length;
-	int request_id;
+	int32_t length;
+	int32_t request_id;
 	enum packet_type type;
-	char *payload;
+	char *payload; /* null-terminated command string */
+	char terminator; /* should always be 0 */
 };
 
-int
-main(int argc, char *argv[])
+void usage();
+
+void
+usage()
 {
+	fputs("usage: " PROGRAM_NAME " -H host -p port -P password\n", stderr);
+}
+
+int main(int argc, char *argv[])
+{
+	char *host = "localhost";
+	uint16_t port = 25575;
+	char *password = "";
+
+	if (argc >= 2 && !strcmp(argv[1], "--help")) {
+		usage();
+		return 0;
+	}
+
+	int optchar;
+	while ((optchar = getopt(argc, argv, "H:p:P:")) != -1) {
+		switch (optchar) {
+		case 'H':
+			host = optarg;
+			break;
+		case 'p':
+			port = (uint16_t)strtoul(optarg, NULL, 10);
+			break;
+		case 'P':
+			password = optarg;
+			break;
+		default:
+			usage();
+			return 1;
+		}
+	}
+
+
 	return 0;
 }
