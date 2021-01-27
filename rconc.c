e4402e8466fb2d1e45ce4bfec6798d324cb6d2cf
Kian Kasad <kian@kasad.com>
2021-01-26 21:19:33 -0800

    update copyright/license

--- rconc.c
+++ rconc.c
@@ -1,6 +1,6 @@
 /*
  * rconc - a simple RCON client
- * Copyright (C) 2020  Kian Kasad
+ * Copyright © 2020-2021 Kian Kasad
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -14,7 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
 
 #include <errno.h>
